46f5eee672391e28c97abaf050de8ec4a1b93c0d
Daniel <dm375211@gmail.com>
2023-01-17 16:34:15 -0500

    asd

---
+++ index.js
@@ -0,0 +1,95 @@
+const width = 28;
+const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
+let squares = []
+
+//28 * 28 = 784; 
+
+/* 
+    0 - pac-dots
+    1 - wall
+    2 - ghost-lair
+    3 - power-pellet
+    4 - empty
+*/
+
+
+const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+  ]
+
+function createBoard() {
+    for(let i = 0; i <layout.length; i++) {
+        const square = document.createElement("div");
+
+        grid.appendChild(square);
+        squares.push(square)  
+        
+        
+        if (layout[i] === 0) {
+            squares[i].classList.add("pac-dot")
+        } else if (layout[i] === 1) {
+            squares[i].classList.add("wall")
+        } else if (layout[i] === 3) {
+            squares[i].classList.add("power-pellet")
+        } 
+        else if (layout[i] === 2) {
+            squares[i].classList.add("ghost-lair")
+        }
+    }
+}
+
+createBoard();
+
+let pacmanCurrentIndex = 490 ;
+
+squares[pacmanCurrentIndex].classList.add("pacman")
+
+function control(event) {
+    squares[pacmanCurrentIndex].classList.remove("pacman")
+    switch(event.key) {
+        case "ArrowUp": 
+            if (!squares[pacmanCurrentIndex - width].classList.contains("wall") && pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width;
+            console.log(pacmanCurrentIndex);
+            break
+        case "ArrowRight": 
+            if (!squares[pacmanCurrentIndex + 1].classList.contains("wall") && pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1
+            break
+        case "ArrowLeft":
+            if (!squares[pacmanCurrentIndex - 1].classList.contains("wall") && pacmanCurrentIndex % width !== 0)pacmanCurrentIndex -= 1;
+            break
+        case "ArrowDown":
+            if (!squares[pacmanCurrentIndex + width].classList.contains("wall") && pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+            break
+    }
+    squares[pacmanCurrentIndex].classList.add("pacman")
+}
+
+document.addEventListener("keyup",control);
